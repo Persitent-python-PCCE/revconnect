82b3f4efae077a0ea400f1c3b57c9475c55fb751
PRAJYOTRane <prajyotrane111@gmail.com>
2026-09-26 21:34:02 +0530

    feat: notify followers when post is created

--- src/main/java/com/revconnect/revconnect/connection/repository/FollowRepository.java
+++ src/main/java/com/revconnect/revconnect/connection/repository/FollowRepository.java
@@ -5,17 +5,32 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
-    Optional<Follow> findByFollowerIdAndFollowingId(Long followerId, Long followingId);
+    Optional<Follow> findByFollowerIdAndFollowingId(
+            Long followerId,
+            Long followingId
+    );
 
-    boolean existsByFollowerIdAndFollowingId(Long followerId, Long followingId);
+    boolean existsByFollowerIdAndFollowingId(
+            Long followerId,
+            Long followingId
+    );
 
-    Page<Follow> findByFollowingId(Long followingId, Pageable pageable);
+    Page<Follow> findByFollowingId(
+            Long followingId,
+            Pageable pageable
+    );
 
-    Page<Follow> findByFollowerId(Long followerId, Pageable pageable);
+    List<Follow> findByFollowingId(Long followingId);
+
+    Page<Follow> findByFollowerId(
+            Long followerId,
+            Pageable pageable
+    );
 
     long countByFollowingId(Long followingId);
 
